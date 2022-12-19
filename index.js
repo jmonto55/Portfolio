fa3d2b7960ef37f0dceddeeab38f906032a9a676
jmonto55 <jmonto55@gmail.com>
2022-12-19 16:07:08 -0500

    Linters debugging #1

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const hamburgerButton = document.querySelector('.hamburger');
 const closeButton = document.querySelector('.close-button');
 const navItems = document.querySelector('.nav_list');
 const myName = document.querySelector('.logo');
-const mailButton = document.getElementById('mail');
 
 function openMenu() {
   hamburgerButton.classList.toggle('invisible');
